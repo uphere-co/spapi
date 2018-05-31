82606918de389d6f448ac25aa4e5a1b718cbb605
Ian-Woo Kim <ianwookim@gmail.com>
2018-05-31 05:26:13 +0000

    success in using etag

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -10,9 +10,9 @@ import Servant.Server (serve)
 import Network.Wai.Handler.Warp (run)
 -- import Network.Wai.Middleware.Static (static,staticPolicy,addBase)
 -- import Network.Wai.Application.Static (staticApp, defaultWebAppSettings)
-import Network.Wai.Middleware.Etag
+import Network.Wai.Middleware.ETag
 
-staticRoot = staticApp . defaultWebAppSettings
+-- staticRoot = staticApp . defaultWebAppSettings
 
 
 main = do
@@ -20,7 +20,7 @@ main = do
   putStrLn "Serving on localhost:8080/static/, visit http://localhost:8080/static/index.html"
   -- run 8080 $ static $
   --   serve api (serveDirectory d)
-  etagcontext <- defaultEtagContext False
+  etagcontext <- defaultETagContext False
   run 3456 $ etag etagcontext NoMaxAge $ serve api (serveDirectory d)
 
 
--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -11,7 +11,7 @@ main = mainWidget bodyElement
 
 bodyElement :: MonadWidget t m => m ()
 bodyElement = do
-  rec el "h2" $ text "Counter as a fold"
+  rec el "h2" $ text "Counter As a FOLD"
       counts <- foldDyn (+) (0 :: Int) $
                   leftmost [ 1 <$ evIncr, -1 <$ evDecr ]
       el "div" $ display counts
